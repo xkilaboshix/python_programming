797fac05f0c177340ebd67342e00bc08326cf161
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-02 04:37:33 +0900

    6-71

--- lesson.py
+++ lesson.py
@@ -1,12 +1,19 @@
-# import lesson_package.utils
-from lesson_package.talk import human
-from lesson_package.talk import animal
+# # import lesson_package.utils
+# from lesson_package.talk import human
+# from lesson_package.talk import animal
+#
+# print(animal.sing())
+# print(animal.cry())
+#
+# # r = utils.say_twice('hello')
+# # print(r)
+#
+# print(human.sing())
+# print(human.cry())
 
-print(animal.sing())
-print(animal.cry())
+try:
+    from lesson_package import utils
+except ImportError:
+    from lesson_package.tools import utils
 
-# r = utils.say_twice('hello')
-# print(r)
-
-print(human.sing())
-print(human.cry())
+utils.say_twice('word')
