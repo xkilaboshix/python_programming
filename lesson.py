862031ed7886ecda24aab9936e948131dcfc977d
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-02 04:09:38 +0900

    fix

--- lesson.py
+++ lesson.py
@@ -1,5 +1,9 @@
 # import lesson_package.utils
 from lesson_package.talk import human
+from lesson_package.talk import animal
+
+print(animal.sing())
+print(animal.cry())
 
 # r = utils.say_twice('hello')
 # print(r)
