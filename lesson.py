fdb98e18a74ba330b4b120fcee91d018078431d9
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-02 19:07:09 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,26 +1,5 @@
-s = "fdjddosffkeosakfjffkkwwwiiee"
+from termcolor import colored
 
-d = {}
-for c in s:
-    if c not in d:
-        d[c] = 0
-    d[c] += 1
-print(d)
+print('test')
 
-d = {}
-for c in s:
-    d.setdefault(c, 0)
-    d[c] += 1
-print(d)
-
-from collections import defaultdict
-
-d = defaultdict(int)
-
-for c in s:
-    d[c] += 1
-print(d)
-
-print(d['w'])
-
-
+print(colored('test', 'red'))
