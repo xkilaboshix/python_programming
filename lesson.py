f0d866f37ff8f5aab6a8b507d88363efb63f4561
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-05 02:33:41 +0900

    up

--- lesson.py
+++ lesson.py
@@ -9,25 +9,33 @@ class ToyotaCar(Car):
         print('fast')
 
 class TeslaCar(Car):
-    def __init__(self, model='Model S', enable_auto_run=False):
-        # self.model = model
+    def __init__(self, model='Model S',
+                 enable_auto_run=False):
         super().__init__(model)
-        self.enable_auto_run = enable_auto_run
+        self.__enable_auto_run = enable_auto_run
+
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        self._enable_auto_run = is_enable
+
     def run(self):
         print('super fast')
-
     def auto_run(self):
         print('auto run')
 
-car = Car()
-car.run()
-
-print("##############")
-toyota_car = ToyotaCar('Lexas')
-print(toyota_car.model)
-toyota_car.run()
-print("##############")
 tesla_car = TeslaCar('Model S')
-print(tesla_car.model)
-tesla_car.run()
-tesla_car.auto_run()
+tesla_car.__enable_auto_run = "XXXXXXXXXXXXX"
+print(tesla_car.__enable_auto_run)
+
+class T(object):
+    pass
+
+t = T()
+t.name = 'Mike'
+t.age = 20
+print(t.name, t.age)
+
