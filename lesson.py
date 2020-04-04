08a02e57b9b6ac7a44d3030e6f93de07a3e9e033
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-04 19:44:03 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,19 +1,33 @@
 class Car(object):
+    def __init__(self, model=None):
+        self.model = model
     def run(self):
         print('run')
 
 class ToyotaCar(Car):
-    pass
+    def run(self):
+        print('fast')
 
 class TeslaCar(Car):
+    def __init__(self, model='Model S', enable_auto_run=False):
+        # self.model = model
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+    def run(self):
+        print('super fast')
+
     def auto_run(self):
         print('auto run')
 
 car = Car()
 car.run()
 
-toyota_car = ToyotaCar()
+print("##############")
+toyota_car = ToyotaCar('Lexas')
+print(toyota_car.model)
 toyota_car.run()
-
-tesla_car = TeslaCar()
+print("##############")
+tesla_car = TeslaCar('Model S')
+print(tesla_car.model)
+tesla_car.run()
 tesla_car.auto_run()
