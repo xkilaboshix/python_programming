67e57df94b0a4bb5d05a324cf32d91af0ec03de9
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-05 03:51:53 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,12 +1,12 @@
-class Person(object):
+import abc
+
+class Person(metaclass=abc.ABCMeta):
     def __init__(self, age=1):
         self.age = age
 
+    @abc.abstractmethod
     def drive(self):
-        if self.age >= 18:
-            print('ok')
-        else:
-            raise Exception('No drive')
+        pass
 
 class Baby(Person):
     def __init__(self, age=1):
@@ -15,15 +15,22 @@ class Baby(Person):
         else:
             raise ValueError
 
+    def drive(self):
+        raise Exception('No drive')
+
 class Adult(Person):
     def __init__(self, age=18):
         if age >= 18:
             super().__init__(age)
         else:
             raise ValueError
-baby = Baby()
-adult = Adult()
 
+    def drive(self):
+        print('ok')
+baby = Baby()
+# baby.drive()
+adult = Adult()
+# adult.drive()
 class Car(object):
     def __init__(self, model=None):
         self.model = model
@@ -32,5 +39,3 @@ class Car(object):
     def ride(self, person):
         person.drive()
 
-car = Car()
-car.ride(adult)
