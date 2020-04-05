d978a07721a2f81797379404043c7c696269157c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-06 03:05:45 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,21 +1,4 @@
-class word(object):
-
-    def __init__(self, text):
-        self.text = text
-
-    def __str__(self):
-        return 'word!!!!!!!'
-
-    def __len__(self):
-        return len(self.text)
-
-    def __add__(self, word):
-        return self.text.lower() + word.text.lower()
-
-    def __eq__(self, word):
-        return self.text.lower() == word.text.lower()
-
-w = word('test')
-w2 = word('test')
-
-print(w == w2)
+f = open('text.txt', 'w')
+f.write('Test\n')
+print('My', 'name', 'is', 'Mike', sep='#', end='!', file=f)
+f.close()
