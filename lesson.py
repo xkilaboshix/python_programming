7502e6f9eb791486513d6e53356a712574f6b002
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-06 04:22:16 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,2 +1,27 @@
-with open('text.txt', 'w') as f:
-    f.write('Test\n')
+s = """\
+AAA
+BBB
+CCC
+DDD
+"""
+# with open('text.txt', 'w') as f:
+#     f.write(s)
+
+with open('text.txt', 'r') as f:
+  # print(f.read())
+  # while True:
+  #     chunk = 2
+  #     line = f.read(chunk)
+  #     print(line)
+  #     if not line:
+  #         break
+  print(f.tell())
+  print(f.read(1))
+  f.seek(5)
+  print(f.read(1))
+  f.seek(14)
+  print(f.read(1))
+  f.seek(15)
+  print(f.read(1))
+  f.seek(5)
+  print(f.read(1))
