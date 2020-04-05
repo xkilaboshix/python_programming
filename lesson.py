7ba71158d5e27b11fc22905d79c7b46ca147c331
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-05 23:53:49 +0900

    up

--- lesson.py
+++ lesson.py
@@ -2,31 +2,21 @@ class Person(object):
 
     kind = 'human'
 
-    def __init__(self, name):
-        self.name = name
-
-    def who_are_you(self):
-        print(self.name, self.kind)
-
-a = Person('A')
-a.who_are_you()
-b = Person('B')
-b.who_are_you()
-
-class T(object):
-
     def __init__(self):
-        self.words = []
+        self.x = 100
 
-    def add_word(self, word):
-        self.words.append(word)
+    @classmethod
+    def what_is_your_kind(cls):
+        return cls.kind
 
-c = T()
-c.add_word('add 1')
-c.add_word('add 2')
-print(c.words)
+    @staticmethod
+    def about(year):
+        print('about human {}'.format(year))
 
-d = T()
-d.add_word('add 3')
-d.add_word('add 4')
-print(d.words)
+a = Person()
+print(a.what_is_your_kind())
+
+print(Person.kind)
+print(Person.what_is_your_kind())
+
+Person.about(1999)
