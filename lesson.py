c1ea85ab70e48036ea6b8e31f6d2ddf88336bfef
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-07 02:17:26 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,7 +1,14 @@
-import string
+import csv
 
-with open('design/email_template.txt') as f:
-    t = string.Template(f.read())
 
-contents = t.substitute(name='Mike', contents='How are you?')
-print(contents)
+with open('test.csv', 'w') as csv_file:
+    fieldnames = ['Name', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'Name': 'A', 'Count': 1})
+    writer.writerow({'Name': 'B', 'Count': 2})
+
+with open('test.csv', 'r') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row['Name'], row['Count'])
