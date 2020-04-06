af7d7ee00569a8786950bb0c84d70f9b4508f96d
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-07 03:23:55 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,14 +1,31 @@
-import csv
+import os
+import pathlib
+import glob
+import shutil
+
+# print(os.path.exists('text.txt'))
+# print(os.path.isfile('text.txt'))
+# print(os.path.isdir('design'))
+
+# os.rename('text.txt', 'renamed.txt')
+# os.symlink('renamed.txt', 'symlink.txt')
+
+# os.mkdir('test_dir')
+# os.rmdir('test_dir')
+
+# pathlib.Path('empty.txt').touch()
+# os.remove('empty.txt')
+
+# os.mkdir('test_dir')
+# os.mkdir('test_dir/test_dir2')
+# print(os.listdir('test_dir'))
+# pathlib.Path('test_dir/test_dir2/empty.txt').touch()
+# shutil.copy('test_dir/test_dir2/empty.txt',
+#             'test_dir/test_dir2/empty2.txt')
+# print(glob.glob('test_dir/test_dir2/*'))
+# shutil.rmtree('test_dir')
+
+print(os.getcwd())
 
 
-with open('test.csv', 'w') as csv_file:
-    fieldnames = ['Name', 'Count']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerow({'Name': 'A', 'Count': 1})
-    writer.writerow({'Name': 'B', 'Count': 2})
 
-with open('test.csv', 'r') as csv_file:
-    reader = csv.DictReader(csv_file)
-    for row in reader:
-        print(row['Name'], row['Count'])
