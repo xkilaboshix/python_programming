2e3f6d75d0a025f8365ee8e4fb8d5b215a722858
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-08 03:33:00 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,20 +1,26 @@
-import tempfile
+import datetime
 
 
-with tempfile.TemporaryFile(mode='w+') as t:
-    t.write('hello')
-    t.seek(0)
-    print(t.read())
+now = datetime.datetime.now()
+print(now)
+print(now.isoformat())
+print(now.strftime('%d/%m/%y-%H%M%S%f'))
 
-with tempfile.NamedTemporaryFile(delete=False) as t:
-    print(t.name)
-    with open(t.name, 'w+') as f:
-        f.write('test\n')
-        f.seek(0)
-        print(f.read())
+today = datetime.date.today()
+print(today)
+print(now.isoformat())
+print(today.strftime('%d/%m/%y'))
 
-with tempfile.TemporaryDirectory() as td:
-    print(td)
+t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
+print(t)
+print(t.isoformat())
+print(t.strftime('%H_%M_%S_%f'))
 
-temp_dir = tempfile.mkdtemp()
-print(temp_dir)
+print(now)
+# d = datetime.timedelta(weeks=1)
+d = datetime.timedelta(days=365)
+# d = datetime.timedelta(hours=1)
+# d = datetime.timedelta(minutes=1)
+# d = datetime.timedelta(seconds=1)
+# d = datetime.timedelta(microseconds=1)
+print(now - d)
