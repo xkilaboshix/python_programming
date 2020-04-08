d46241f8832518c8866709f4dc42eb35f98de572
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-08 20:25:57 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,45 +1,14 @@
-import datetime
+import subprocess
 
 
-now = datetime.datetime.now()
-print(now)
-print(now.isoformat())
-print(now.strftime('%d/%m/%y-%H%M%S%f'))
+# subprocess.run(['ls', '-al'])
+# r = subprocess.run('lsaaaa', shell=True, check=True)
+# print(r.returncode)
+# print('###')
 
-today = datetime.date.today()
-print(today)
-print(now.isoformat())
-print(today.strftime('%d/%m/%y'))
-
-t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
-print(t)
-print(t.isoformat())
-print(t.strftime('%H_%M_%S_%f'))
-
-print(now)
-# d = datetime.timedelta(weeks=1)
-d = datetime.timedelta(days=365)
-# d = datetime.timedelta(hours=1)
-# d = datetime.timedelta(minutes=1)
-# d = datetime.timedelta(seconds=1)
-# d = datetime.timedelta(microseconds=1)
-print(now - d)
-
-import time
-# print("###")
-# time.sleep(2)
-# print("###")
-print(time.time())
-
-import os
-import shutil
-
-file_name = 'test.txt'
-
-if os.path.exists(file_name):
-    shutil.copy(file_name, "{}.{}".format(
-        file_name, now.strftime('%Y_%m_%d_%H_%M_%S')))
-
-with open(file_name, 'w') as f:
-    f.write('test')
-
+p1 = subprocess.Popen(['ls', '-al'], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(
+    ['grep', 'test'], stdin=p1.stdout, stdout=subprocess.PIPE)
+p1.stdout.close()
+output = p2.communicate()[0]
+print(output)
