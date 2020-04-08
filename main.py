832cc71884ef94812f59656465c2ac779a237cb9
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-09 02:10:32 +0900

    up

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+import os
+import csv
 from termcolor import colored
+from termcolor import cprint
 
 while True:
-    name = input(colored('Hello, I am Roboko. What is your name?\n', 'green'))
+    cprint('=' * 40, 'green')
+    name = input(colored('Hello, I am Roboko. What is your name?\n' + '=' * 40 + '\n', 'green'))
     if name:
-        restaurant = input(colored(name + ', which restaurants do you like?\n', 'green'))
-        if restaurant:
-            fin = colored('Roboko: Thank you so much, {}!\nHave a good day!', 'green')
-            print(fin.format(name))
-            break
+        break
+    cprint('Please enter your name!', 'red')
+
+while True:
+    cprint('=' * 40, 'green')
+    restaurant = input(colored(name + ', which restaurants do you like?\n' + '=' * 40 + '\n', 'green'))
+    cprint('=' * 40, 'green')
+    fin = colored('Roboko: Thank you so much, {}!\nHave a good day!\n' + '=' * 40 + '\n', 'green')
+    print(fin.format(name))
+    if restaurant:
+        with open('ranking.csv', 'a') as csv_file:
+            fieldnames = ['NAME', 'COUNT']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            current_path = os.getcwd()  # カレントディレクトリパスを取得
+            file_name = 'ranking.csv'  # 検索ファイル名を指定
+            file_path = os.path.join(current_path, file_name)
+            myCheck = os.path.isfile(file_path)
+            if not myCheck:
+                writer.writeheader()
+                writer.writerow({'NAME': restaurant, 'COUNT': 1})
+                break
+            else:
+                writer.writerow({'NAME': restaurant, 'COUNT': 1})
+                break
+
+
