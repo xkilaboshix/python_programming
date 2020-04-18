514090352aeaaa8c2a75afecf79af6f1efd36437
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-18 18:05:30 +0900

    up

--- lesson.py
+++ lesson.py
@@ -8,8 +8,9 @@ DEBUG
 import logging
 
 
-logging.basicConfig(filename='test.log', level=logging.DEBUG)
-
+# logging.basicConfig(filename='test.log', level=logging.DEBUG)
+formatter = '%(asctime)s:%(message)s'
+logging.basicConfig(level=logging.INFO, format=formatter)
 
 # logging.critical('critical')
 # logging.error('error')
