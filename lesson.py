a49c448bfc65d9312cb532056128790db0bba699
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-19 16:53:02 +0900

    up

--- lesson.py
+++ lesson.py
@@ -6,6 +6,7 @@ INFO
 DEBUG
 """
 import logging
+
 import logtest
 
 
@@ -15,4 +16,4 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.info('from main')
 
-
+logtest.do_something()
