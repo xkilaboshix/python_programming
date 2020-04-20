81c0c35cfa76a3cedeead92ccc8f1ecd1dc23e80
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-20 23:59:09 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,37 +1,13 @@
-import logging.config
+import logging
 
 
-# logging.config.fileConfig('logging.ini')
-logging.config.dictConfig({
-    'version': 1,
-    'formatters': {
-        'sampleFormatter': {
-            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'}
-    },
-    'handlers': {
-        'sampleHandlers': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'sampleFormatter',
-            'level': logging.DEBUG
-        }
-    },
-    'root': {
-        'handlers': ['sampleHandlers'],
-        'level': logging.WARNING,
-    },
-    'loggers': {
-        'simpleExample': {
-            'handlers': ['sampleHandlers'],
-            'level': logging.DEBUG,
-            'propagate': 0
-        }
-    }
+logger = logging.getLogger(__name__)
+
+
+logger.error('Api call is failed')
+
+logger.error({
+    'action': 'cteate',
+    'status': 'fail',
+    'message': 'Api call is failed'
 })
-
-logger = logging.getLogger('simpleExample')
-
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warn message')
-logger.error('error message')
-logger.critical('critical message')
