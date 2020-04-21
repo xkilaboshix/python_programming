2b0b167d41df47d9a391060a0abc52cc622255c7
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-22 02:43:48 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,13 +1,35 @@
-import logging
+from optparse import OptionParser
+from optparse import OptionGroup
+
+def main():
+
+    usage = 'usage: %prog [options] arg1 arg2'
+    parser = OptionParser(usage=usage)
+    parser.add_option('-f', '--file', action='store', type='string', dest='filename', help='File name')
+    parser.add_option('-n', '--num', action='store', type='int', dest='num')
+    parser.set_defaults(verbose=True)
+    parser.add_option('-v', action='store_true', dest='verbose')
+    # parser.add_option('-v', action='store_false', dest='verbose', default=True)
+    parser.add_option('-q', action='store_false', dest='verbose')
+
+    parser.add_option('-r', action='store_const', const='root', dest='user_name')
+
+    parser.add_option('-e', dest='env')
+
+    def is_release(option, opt_str, value, parser):
+        if parser.values.env == 'prd':
+            raise parser.error("Can`t release")
+        setattr(parser.values, option.dest, True)
+    parser.add_option('--release', action='callback', callback=is_release, dest='release')
+
+    group = OptionGroup(parser, 'Dangerous options')
+    group.add_option('-g', action='store_true', help='Group option')
+    parser.add_option_group(group)
 
 
-logger = logging.getLogger(__name__)
+    options, args = parser.parse_args()
+    print (options)
+    print (args)
 
-
-logger.error('Api call is failed')
-
-logger.error({
-    'action': 'cteate',
-    'status': 'fail',
-    'message': 'Api call is failed'
-})
+if __name__ == '__main__':
+    main()
