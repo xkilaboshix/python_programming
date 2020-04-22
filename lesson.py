67150b96c1ba862d9f0af39d754f7fae57d1b7fa
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-22 15:58:49 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,35 +1,38 @@
-from optparse import OptionParser
-from optparse import OptionGroup
-
-def main():
-
-    usage = 'usage: %prog [options] arg1 arg2'
-    parser = OptionParser(usage=usage)
-    parser.add_option('-f', '--file', action='store', type='string', dest='filename', help='File name')
-    parser.add_option('-n', '--num', action='store', type='int', dest='num')
-    parser.set_defaults(verbose=True)
-    parser.add_option('-v', action='store_true', dest='verbose')
-    # parser.add_option('-v', action='store_false', dest='verbose', default=True)
-    parser.add_option('-q', action='store_false', dest='verbose')
-
-    parser.add_option('-r', action='store_const', const='root', dest='user_name')
-
-    parser.add_option('-e', dest='env')
-
-    def is_release(option, opt_str, value, parser):
-        if parser.values.env == 'prd':
-            raise parser.error("Can`t release")
-        setattr(parser.values, option.dest, True)
-    parser.add_option('--release', action='callback', callback=is_release, dest='release')
-
-    group = OptionGroup(parser, 'Dangerous options')
-    group.add_option('-g', action='store_true', help='Group option')
-    parser.add_option_group(group)
+import sqlite3
 
 
-    options, args = parser.parse_args()
-    print (options)
-    print (args)
+# conn = sqlite3.connect('test_sqlite.db')
+conn = sqlite3.connect(':memory:')
 
-if __name__ == '__main__':
-    main()
+curs = conn.cursor()
+
+curs.execute(
+    'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)'
+)
+conn.commit()
+
+curs.execute(
+    'INSERT INTO persons(name) values("Mike")'
+)
+conn.commit()
+
+
+curs.execute(
+    'INSERT INTO persons(name) values("Nancy")'
+)
+curs.execute(
+    'INSERT INTO persons(name) values("Jun")'
+)
+conn.commit()
+
+curs.execute('UPDATE persons set name = "Michael" WHERE name = "Mike"')
+conn.commit()
+
+curs.execute('DELETE FROM persons WHERE name = "Michael"')
+conn.commit()
+
+curs.execute('SELECT * FROM persons')
+print(curs.fetchall())
+
+curs.close()
+conn.close()
