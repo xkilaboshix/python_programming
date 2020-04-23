cd2641cc7e80f6d5e93c6aa72dbdfc1224f141b2
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-23 19:16:23 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,40 +1,9 @@
-import sqlalchemy
-import sqlalchemy.ext.declarative
-import sqlalchemy.orm
+import dbm
 
-engine = sqlalchemy.create_engine('mysql+pymysql://{user}:{password}@{host}/{db-name}?charset=utf8', echo=True)
 
-Base = sqlalchemy.ext.declarative.declarative_base()
+# with dbm.open('cache', 'c') as db:
+#     db['key1'] = 'value1'
+#     db['key2'] = 'value2'
 
-class Person(Base):
-    __tablename__ = 'persons'
-    id = sqlalchemy.Column(
-        sqlalchemy.Integer, primary_key=True, autoincrement=True
-    )
-    name = sqlalchemy.Column(sqlalchemy.String(14))
-
-Base.metadata.create_all(engine)
-
-Session = sqlalchemy.orm.sessionmaker(bind=engine)
-session = Session()
-
-p1 = Person(name='Mike')
-session.add(p1)
-p2 = Person(name='Nancy')
-session.add(p2)
-p3 = Person(name='Jun')
-session.add(p3)
-session.commit()
-
-p4 = session.query(Person).filter_by(name='Mike').first()
-p4.name = 'Michel'
-session.add(p4)
-session.commit()
-
-p5 = session.query(Person).filter_by(name='Nancy').first()
-session.delete(p5)
-session.commit()
-
-persons = session.query(Person).all()
-for person in persons:
-    print(person.id, person.name)
+with dbm.open('cache', 'r') as db:
+    print(db.get('key2'))
