fb32bc86c03d441741d34abc715bcb34871a6657
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-23 16:03:40 +0900

    up

--- lesson.py
+++ lesson.py
@@ -18,8 +18,17 @@ Base.metadata.create_all(engine)
 Session = sqlalchemy.orm.sessionmaker(bind=engine)
 session = Session()
 
-person = Person(name='Mike')
-session.add(person)
+p1 = Person(name='Mike')
+session.add(p1)
+p2 = Person(name='Nancy')
+session.add(p2)
+p3 = Person(name='Jun')
+session.add(p3)
+session.commit()
+
+p4 = session.query(Person).filter_by(name='Mike').first()
+p4.name = 'Michel'
+session.add(p4)
 session.commit()
 
 persons = session.query(Person).all()
