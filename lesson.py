f8acbafc1d5dfc46579c0b9b4d2a4e4193feb506
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-25 01:38:40 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,9 +1,31 @@
-import dbm
+import sqlite3
+import time
+import memcache
 
+db = memcache.Client(['127.0.0.1:11211'])
 
-# with dbm.open('cache', 'c') as db:
-#     db['key1'] = 'value1'
-#     db['key2'] = 'value2'
+# db.set('web_page', 'value1', time=3)
+# time.sleep(1)
+# print(db.get('web_page'))
 
-with dbm.open('cache', 'r') as db:
-    print(db.get('key2'))
+# db.set('counter', 0)
+# db.incr('counter', 1)
+# db.incr('counter', 1)
+# db.incr('counter', 1)
+# db.incr('counter', 1)
+# print(db.get('counter'))
+
+conn = sqlite3.connect('test_sqlite2.db')
+curs = conn.cursor()
+# curs.execute(
+#     'CREATE TABLE persons('
+#     'employ_id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)')
+# curs.execute('INSERT INTO persons(name) values("Mike")')
+# conn.commit()
+# conn.close()
+
+def get_employ_id(name):
+    employ_id = db.get(name)
+    if employ_id:
+        return employ_id
+    curs.execute()
