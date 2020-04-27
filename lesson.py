eca697d32c225109ac3ce6d652cc7e1d8acbaf7c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-27 16:54:21 +0900

    up

--- lesson.py
+++ lesson.py
@@ -27,4 +27,12 @@ db_stacks = db.stacks
 # from bson.objectid import ObjectId
 # str_stack_id = '5ea68b2ee8f355b69d1c800f'
 # print(db_stacks.find_one({'_id': ObjectId(str_stack_id)}))
-print(db_stacks.find_one({'pip': ['python', 'java', 'go']}))
+# print(db_stacks.find_one({'pip': ['python', 'java', 'go']}))
+# stack_id = db_stacks.insert_one(stack2).inserted_id
+# print(stack_id, type(stack_id))
+#
+# for stack in db_stacks.find():
+#     print(stack)
+now = datetime.datetime.utcnow()
+for stack in db_stacks.find({'data': {'$lt': now}}):
+    print(stack)
