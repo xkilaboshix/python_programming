93cc1aea1b9a732a75682f4b3bdf9e4e89bb7458
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-02 03:04:51 +0900

    up

--- lesson.py
+++ lesson.py
@@ -13,7 +13,22 @@ import urllib.request
 import json
 
 
-url = 'http://httpbin.org/get'
-with urllib.request.urlopen(url) as f:
-    r = json.loads(f.read().decode('utf-8'))
-    print(type(r))
+payload = {'key1': 'value1', 'key2': 'value2'}
+
+# url = 'http://httpbin.org/get' + '?' + urllib.parse.urlencode(payload)
+# with urllib.request.urlopen(url) as f:
+#     r = json.loads(f.read().decode('utf-8'))
+
+payload = json.dumps(payload).encode('utf-8')
+# req = urllib.request.Request('http://httpbin.org/post', data=payload, method='POST')
+# with urllib.request.urlopen(req) as f:
+#     print(json.loads(f.read().decode('utf-8')))
+
+req = urllib.request.Request('http://httpbin.org/put', data=payload, method='PUT')
+with urllib.request.urlopen(req) as f:
+    print(json.loads(f.read().decode('utf-8')))
+
+req = urllib.request.Request('http://httpbin.org/delete', data=payload, method='DELETE')
+with urllib.request.urlopen(req) as f:
+    print(json.loads(f.read().decode('utf-8')))
+
