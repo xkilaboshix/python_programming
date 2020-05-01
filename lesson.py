948e671f969ff5cb022f68f0a2df60984a20a3f5
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-02 02:48:29 +0900

    up

--- lesson.py
+++ lesson.py
@@ -15,4 +15,5 @@ import json
 
 url = 'http://httpbin.org/get'
 with urllib.request.urlopen(url) as f:
-    print(json.loads(f.read().decode('utf-8')))
+    r = json.loads(f.read().decode('utf-8'))
+    print(type(r))
