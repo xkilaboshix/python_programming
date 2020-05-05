36b1062c43c6bd3bb41b5f6cd8e865eb1c3e7e2c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-05 22:26:12 +0900

    up

--- socket_client.py
+++ socket_client.py
@@ -1,8 +1,11 @@
 import socket
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('127.0.0.1', 50007))
-    s.sendall(b'Hello')
-    data = s.recv(1024)
-    print(repr(data))
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect(('127.0.0.1', 50007))
+#     s.sendall(b'Hello')
+#     data = s.recv(1024)
+#     print(repr(data))
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.sendto(b'Hello UDP', ('127.0.0.1', 50007))
--- socket_server.py
+++ socket_server.py
@@ -7,15 +7,22 @@
 import socket
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind(('127.0.0.1', 50007))
+#     s.listen(1)
+#     while True:
+#         conn, addr = s.accept()
+#         with conn:
+#             while True:
+#                 data = conn.recv(1024)
+#                 if not data:
+#                     break
+#                 print('data: {}, addr: {}'.format(data, addr))
+#                 conn.sendall(b'Received: ' + data)
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind(('127.0.0.1', 50007))
-    s.listen(1)
     while True:
-        conn, addr = s.accept()
-        with conn:
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print('data: {}, addr: {}'.format(data, addr))
-                conn.sendall(b'Received: ' + data)
+        data, addr = s.recvfrom(1024)
+        print("data: {}, addr: {}".format(data, addr))
+
