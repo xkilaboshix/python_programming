a42b01360bae7fc8a315dfbb8c32916d73e12a3f
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-06 16:16:56 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,7 +1,25 @@
-import http.server
-import socketserver
+from flask import  Flask
+from flask import g
+from flask import render_template
+from flask import request
+from flask import Response
 
 
-with socketserver.TCPServer(('127.0.0.1', 8000),
-                            http.server.SimpleHTTPRequestHandler) as httpd:
-    httpd.serve_forever()
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'top'
+
+@app.route('/hello')
+def hello_world2():
+    return 'hello world!'
+
+def main():
+    app.debug = True
+    app.run()
+    # app.run(host='127.0.0.1', port=5000)
+
+if __name__ == '__main__':
+    main()
