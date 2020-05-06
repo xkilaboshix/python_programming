1c0e513384b6cd1cd1153632ef57e208459ce85e
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-06 18:37:43 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,4 +1,6 @@
-from flask import  Flask
+import sqlite3
+
+from flask import Flask
 from flask import g
 from flask import render_template
 from flask import request
@@ -6,6 +8,58 @@ from flask import Response
 
 
 app = Flask(__name__)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('test_sqlite.db')
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/employee', methods=['POST', 'PUT', 'DELETE'])
+@app.route('/employee/<name>', methods=['GET'])
+def employee(name=None):
+    db = get_db()
+    curs = db.cursor()
+    curs.execute(
+        'CREATE TABLE IF NOT EXISTS persons( '
+        'id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)'
+    )
+
+    name = request.values.get('name', name)
+    if request.method == 'GET':
+        curs.execute('SELECT * FROM persons WHERE name ="{}"'.format(name))
+        person = curs.fetchone()
+        if not person:
+            return "No", 404
+        user_id, name = person
+        return '{}:{}'.format(user_id, name), 200
+
+    if request.method == 'POST':
+        curs.execute('INSERT INTO persons(name) values("{}")'.format(name))
+        db.commit()
+        return 'created {}'.format(name), 201
+
+    if request.method == 'PUT':
+        new_name = request.values['new_name']
+        curs.execute('UPDATE persons set name = "{}" WHERE name = "{}"'.format(
+            new_name, name
+        ))
+        db.commit()
+        return 'updated {}: {}'.format(name, new_name), 200
+
+    if request.method == 'DELETE':
+        curs.execute('DELETE from persons WHERE name = "{}"'.format(name))
+        db.commit()
+        return 'deleted {}'.format(name), 200
+
+    curs.close()
 
 
 @app.route('/')
--- test_flask.py
+++ test_flask.py
@@ -1,7 +1,15 @@
 import requests
 
 
-r = requests.post(
-    'http://127.0.0.1:5000/post', data={'username': 'Mike'}
-)
+r = requests.get( 'http://127.0.0.1:5000/employee/jun')
+print(r.text)
+
+r = requests.post( 'http://127.0.0.1:5000/employee', data={'name': 'jun'})
+print(r.text)
+
+r = requests.put( 'http://127.0.0.1:5000/employee', data={
+    'name': 'jun', 'new_name': 'sakai'})
+print(r.text)
+
+r = requests.delete( 'http://127.0.0.1:5000/employee', data={'name': 'jun'})
 print(r.text)
