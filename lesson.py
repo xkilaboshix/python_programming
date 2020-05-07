fa098d75eb7e6e164d13fee48007efade12fc51c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-08 01:15:27 +0900

    up

--- lesson.py
+++ lesson.py
@@ -5,8 +5,11 @@ import networkx as nx
 G = nx.Graph()
 G.add_node(1)
 
-G.add_edge(1, 2)
-G.add_edge(2, 3)
+G.add_nodes_from([2, 3])
+# G.add_edge(1, 2)
+# G.add_edge(2, 3)
+
+G.add_edges_from([(1, 2), (2, 3)])
 
 nx.draw(G)
 plt.show()
