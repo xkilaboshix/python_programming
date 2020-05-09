bbc1d0034f4363f812448bbea5213aa04701d160
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-09 19:42:50 +0900

    up

--- conftest.py
+++ conftest.py
@@ -1,2 +1,9 @@
+import pytest
+
+
+@pytest.fixture
+def csv_file():
+    return 'csv_file!!!'
+
 def pytest_addoption(parser):
     parser.addoption('--os-name', default='kinux', help='os name')
--- test_calculation.py
+++ test_calculation.py
@@ -10,8 +10,8 @@ class TestCal(object):
         cls.cal = calculation.Cal()
         cls.test_file_name = 'test.txt'
 
-    def test_add_num_and_double(self, tmpdir):
-        print(tmpdir)
+    def test_add_num_and_double(self, csv_file):
+        print(csv_file)
         assert self.cal.add_num_and_double(1, 1) == 4
 
     def test_save(self, tmpdir):
