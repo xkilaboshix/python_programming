5c517bb5845e98fb3a2628a41e637ff5aeb4e4a9
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-09 18:21:06 +0900

    up

--- test_calculation.py
+++ test_calculation.py
@@ -1,25 +1,38 @@
 import calculation
+import pytest
 
 class TestCal(object):
+
+    @classmethod
+    def setup_class(cls):
+        print('start')
+        cls.cal = calculation.Cal()
+
+    @classmethod
+    def teardown_class(cls):
+        print('end')
+        del cls.cal
+
+    def setup_method(self, method):
+        print('method={}'.format(method.__name__))
+        # self.cal = calculation.Cal()
+
+    def teardown_method(self, method):
+        print('method={}'.format(method.__name__))
+        # del self.cal
+
     def test_add_num_and_double(self):
-        cal = calculation.Cal()
-        assert cal.add_num_and_double(1, 1) == 4
+        assert self.cal.add_num_and_double(1, 1) == 4
+
+    def test_add_num_and_double_raise(self):
+        with pytest.raises(ValueError):
+            self.cal.add_num_and_double('1', '1')
 
 # release_name = 'lesson2'
 #
-# class CalTest(unittest.TestCase):
-#     def setUp(self):
-#         print('setup')
-#         self.cal = calculation.Cal()
-#
-#     def tearDown(self):
-#         print('clean up')
-#         del self.cal
+
 #
 #     @unittest.skipIf(release_name=='lesson2', 'skip!!')
 #     def test_add_num_and_double(self):
 #         self.assertEqual(self.cal.add_num_and_double(1, 1), 4)
 #
-#     def test_add_num_and_double_raise(self):
-#         with self.assertRaises(ValueError):
-#             self.cal.add_num_and_double('1', '1')
