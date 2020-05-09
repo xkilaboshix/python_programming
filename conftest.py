6771fab45179e9890d838ad6397c344ff85e7ef3
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-09 19:56:02 +0900

    up

--- conftest.py
+++ conftest.py
@@ -3,7 +3,10 @@ import pytest
 
 @pytest.fixture
 def csv_file():
-    return 'csv_file!!!'
+    with open('test.csv', 'w') as c:
+        print('before test')
+        yield c
+        print('after test')
 
 def pytest_addoption(parser):
     parser.addoption('--os-name', default='kinux', help='os name')
