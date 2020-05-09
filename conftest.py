669bd0f3e5d0a7e1d7be338f3c4ed41fe2e0d622
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-09 18:59:23 +0900

    up

---
+++ conftest.py
@@ -0,0 +1,2 @@
+def pytest_addoption(parser):
+    parser.addoption('--os-name', default='kinux', help='os name')
--- test_calculation.py
+++ test_calculation.py
@@ -1,5 +1,5 @@
-import calculation
 import pytest
+import calculation
 
 
 is_release = True
@@ -7,32 +7,14 @@ class TestCal(object):
 
     @classmethod
     def setup_class(cls):
-        print('start')
         cls.cal = calculation.Cal()
 
-    @classmethod
-    def teardown_class(cls):
-        print('end')
-        del cls.cal
-
-    def setup_method(self, method):
-        print('method={}'.format(method.__name__))
-        # self.cal = calculation.Cal()
-
-    def teardown_method(self, method):
-        print('method={}'.format(method.__name__))
-        # del self.cal
-
-    def test_add_num_and_double(self):
+    def test_add_num_and_double(self, request):
+        os_name = request.config.getoption('--os-name')
+        print(os_name)
+        if os_name == 'mac':
+            print('ls')
+        elif os_name == 'windows':
+            print('dir')
         assert self.cal.add_num_and_double(1, 1) == 4
 
-    # @pytest.mark.skip(reason='skip')
-    @pytest.mark.skipif(is_release==True, reason='skip!')
-    def test_add_num_and_double_raise(self):
-        with pytest.raises(ValueError):
-            self.cal.add_num_and_double('1', '1')
-
-#     @unittest.skipIf(release_name=='lesson2', 'skip!!')
-#     def test_add_num_and_double(self):
-#         self.assertEqual(self.cal.add_num_and_double(1, 1), 4)
-#
