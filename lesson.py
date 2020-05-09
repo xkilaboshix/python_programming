86e0f98aef9af43f22c2e27858af6e8740ead527
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-09 16:59:23 +0900

    up

--- lesson.py
+++ lesson.py
@@ -5,7 +5,14 @@ class Cal(object):
         >>> c = Cal()
         >>> c.add_num_and_double(1, 1)
         4
+
+        >>> c.add_num_and_double('1', '1')
+        Traceback (most recent call last):
+        ...
+        ValueError
         """
+        if type(x) is not int or type(y) is not int:
+            raise ValueError
 
         result = x + y
         result *= 2
