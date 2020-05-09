e55dd71f1019336403e6cd826563ef6b712d6e90
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-09 17:38:23 +0900

    up

--- test_calculation.py
+++ test_calculation.py
@@ -2,6 +2,8 @@ import unittest
 
 import calculation
 
+release_name = 'lesson2'
+
 class CalTest(unittest.TestCase):
     def setUp(self):
         print('setup')
@@ -11,7 +13,7 @@ class CalTest(unittest.TestCase):
         print('clean up')
         del self.cal
 
-    @unittest.skip('skip!')
+    @unittest.skipIf(release_name=='lesson2', 'skip!!')
     def test_add_num_and_double(self):
         self.assertEqual(self.cal.add_num_and_double(1, 1), 4)
 
