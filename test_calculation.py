a9ba48054a2b4fa1709a91cb3b5012449bd0d30b
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-09 17:33:38 +0900

    up

--- test_calculation.py
+++ test_calculation.py
@@ -3,11 +3,17 @@ import unittest
 import calculation
 
 class CalTest(unittest.TestCase):
+    def setUp(self):
+        print('setup')
+        self.cal = calculation.Cal()
+
+    def tearDown(self):
+        print('clean up')
+        del self.cal
+
     def test_add_num_and_double(self):
-        cal = calculation.Cal()
-        self.assertEqual(cal.add_num_and_double(1, 1), 4)
+        self.assertEqual(self.cal.add_num_and_double(1, 1), 4)
 
     def test_add_num_and_double_raise(self):
-        cal = calculation.Cal()
         with self.assertRaises(ValueError):
-            cal.add_num_and_double('1', '1')
+            self.cal.add_num_and_double('1', '1')
