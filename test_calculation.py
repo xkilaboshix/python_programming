1f1d4853007410d50ed64baf4d785a7e9fb56ee9
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-09 17:46:08 +0900

    up

--- test_calculation.py
+++ test_calculation.py
@@ -1,22 +1,24 @@
-import unittest
-
 import calculation
 
-release_name = 'lesson2'
+def test_add_num_and_double():
+    cal = calculation.Cal()
+    assert cal.add_num_and_double(1, 1) == 4
 
-class CalTest(unittest.TestCase):
-    def setUp(self):
-        print('setup')
-        self.cal = calculation.Cal()
-
-    def tearDown(self):
-        print('clean up')
-        del self.cal
-
-    @unittest.skipIf(release_name=='lesson2', 'skip!!')
-    def test_add_num_and_double(self):
-        self.assertEqual(self.cal.add_num_and_double(1, 1), 4)
-
-    def test_add_num_and_double_raise(self):
-        with self.assertRaises(ValueError):
-            self.cal.add_num_and_double('1', '1')
+# release_name = 'lesson2'
+#
+# class CalTest(unittest.TestCase):
+#     def setUp(self):
+#         print('setup')
+#         self.cal = calculation.Cal()
+#
+#     def tearDown(self):
+#         print('clean up')
+#         del self.cal
+#
+#     @unittest.skipIf(release_name=='lesson2', 'skip!!')
+#     def test_add_num_and_double(self):
+#         self.assertEqual(self.cal.add_num_and_double(1, 1), 4)
+#
+#     def test_add_num_and_double_raise(self):
+#         with self.assertRaises(ValueError):
+#             self.cal.add_num_and_double('1', '1')
