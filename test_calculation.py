65ebfc7f6c61bccc966c4ade61e8e631cdfa5a55
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-09 17:50:00 +0900

    up

--- test_calculation.py
+++ test_calculation.py
@@ -1,8 +1,9 @@
 import calculation
 
-def test_add_num_and_double():
-    cal = calculation.Cal()
-    assert cal.add_num_and_double(1, 1) == 4
+class TestCal(object):
+    def test_add_num_and_double(self):
+        cal = calculation.Cal()
+        assert cal.add_num_and_double(1, 1) == 4
 
 # release_name = 'lesson2'
 #
