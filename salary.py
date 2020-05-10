329eed677d2a37f4f4c0e4ba64650c64db66a32c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-11 00:48:28 +0900

    up

---
+++ salary.py
@@ -0,0 +1,16 @@
+import requests
+
+class ThirdPartyBonusRestApi(object):
+    def bonus_price(self, year):
+        r = requests.get('http://localhost/bonus', params={'year':year})
+        return r.json()['price']
+
+class Salary(object):
+    def __init__(self, base=100, year=2017):
+        self.bonus_api = ThirdPartyBonusRestApi()
+        self.base = base
+        self.year = year
+
+    def calculation_salary(self):
+        bonus = self.bonus_api.bonus_price(year=self.year)
+        return self.base + bonus
---
+++ test_salary.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest.mock import MagicMock
+import salary
+
+
+class TestSalary(unittest.TestCase):
+    def test_calculation_salary(self):
+        s = salary.Salary(year=2017)
+        s.bonus_api.bonus_price = MagicMock(return_value=1)
+        self.assertEqual(s.calculation_salary(), 101)
