eeeba594ad4dc69a97ae1268ccbc607afe962d0c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-11 00:59:24 +0900

    up

--- test_salary.py
+++ test_salary.py
@@ -8,3 +8,7 @@ class TestSalary(unittest.TestCase):
         s = salary.Salary(year=2017)
         s.bonus_api.bonus_price = MagicMock(return_value=1)
         self.assertEqual(s.calculation_salary(), 101)
+        s.bonus_api.bonus_price.assert_called_once()
+        s.bonus_api.bonus_price.assert_called_with(year=2017)
+        s.bonus_api.bonus_price.assert_called_once_with(year=2017)
+        self.assertEqual(s.bonus_api.bonus_price.call_count, 1)
