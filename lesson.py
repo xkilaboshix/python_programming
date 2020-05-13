fa5d1650e5d7aae27b8837fc6fc761213d1bf763
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-13 18:46:32 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,20 +1,25 @@
+import logging
 import threading
 import time
 
+logging.basicConfig(
+    level=logging.DEBUG, format='%(threadName)s: %(message)s')
 
 def worker1():
-    print(threading.currentThread().getName(), 'start')
+    logging.debug('start')
     time.sleep(5)
-    print(threading.currentThread().getName(), 'end')
+    logging.debug('end')
 
 def worker2():
-    print(threading.currentThread().getName(), 'start')
-    time.sleep(5)
-    print(threading.currentThread().getName(), 'end')
+    logging.debug('start')
+    time.sleep(2)
+    logging.debug('end')
 
 if __name__ == '__main__':
     t1 = threading.Thread(target=worker1)
+    t1.setDaemon(True)
     t2 = threading.Thread(target=worker2)
     t1.start()
     t2.start()
     print('started')
+    t1.join()
