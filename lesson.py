748e948341894fcae82a1f5278e02badb7c49c11
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-14 18:13:05 +0900

    up

--- lesson.py
+++ lesson.py
@@ -5,23 +5,29 @@ import time
 logging.basicConfig(
     level=logging.DEBUG, format='%(threadName)s: %(message)s')
 
-def worker1(x, y=1):
+def worker1(d, lock):
     logging.debug('start')
-    logging.debug(x)
-    logging.debug(y)
+    lock.acquire()
+    i = d['x']
     time.sleep(5)
+    d['x'] = i + 1
+    logging.debug(d)
+    lock.release()
     logging.debug('end')
 
-def worker2():
+def worker2(d, lock):
     logging.debug('start')
-    time.sleep(2)
+    lock.acquire()
+    i = d['x']
+    d['x'] = i + 1
+    logging.debug(d)
+    lock.release()
     logging.debug('end')
 
 if __name__ == '__main__':
-    t = threading.Timer(3, worker1, args=(100,), kwargs={'y':200})
-    t.start()
-    # t2 = threading.Thread(target=worker2)
-    # t1.start()
-    # t2.start()
-    # print('started')
-    # t1.join()
+    d = {'x': 0}
+    lock = threading.Lock()
+    t1 = threading.Thread(target=worker1, args=(d, lock))
+    t2 = threading.Thread(target=worker2, args=(d, lock))
+    t1.start()
+    t2.start()
