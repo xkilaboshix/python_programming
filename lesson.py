b9b3f844a70da87937de89a1b070685f02daa372
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-14 23:52:15 +0900

    up

--- lesson.py
+++ lesson.py
@@ -5,29 +5,32 @@ import time
 logging.basicConfig(
     level=logging.DEBUG, format='%(threadName)s: %(message)s')
 
-def worker1(event):
-    event.wait()
-    logging.debug('start')
-    time.sleep(3)
-    logging.debug('end')
+def worker1(condition):
+    with condition:
+        condition.wait()
+        logging.debug('start')
+        time.sleep(3)
+        logging.debug('end')
 
-def worker2(event):
-    event.wait()
-    logging.debug('start')
-    time.sleep(3)
-    logging.debug('end')
+def worker2(condition):
+    with condition:
+        condition.wait()
+        logging.debug('start')
+        time.sleep(3)
+        logging.debug('end')
 
-def worker3(event):
-    logging.debug('start')
-    time.sleep(5)
-    logging.debug('end')
-    event.set()
+def worker3(condition):
+    with condition:
+        logging.debug('start')
+        time.sleep(5)
+        logging.debug('end')
+        condition.notifyAll()
 
 if __name__ == '__main__':
-    event = threading.Event()
-    t1 = threading.Thread(target=worker1, args=(event,))
-    t2 = threading.Thread(target=worker2, args=(event,))
-    t3 = threading.Thread(target=worker3, args=(event,))
+    condition = threading.Condition()
+    t1 = threading.Thread(target=worker1, args=(condition,))
+    t2 = threading.Thread(target=worker2, args=(condition,))
+    t3 = threading.Thread(target=worker3, args=(condition,))
     t1.start()
     t2.start()
     t3.start()
