58ac3085ffb980eae3604973ee8d472bff9e52dd
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-15 01:46:12 +0900

    up

--- lesson.py
+++ lesson.py
@@ -14,12 +14,14 @@ def worker1(i):
 if __name__ == '__main__':
     # t1 = multiprocessing.Process(target=worker1, args=(i,))
     with multiprocessing.Pool(3) as p:
-        logging.debug(p.apply(worker1, (200, )))
-        logging.debug('executed apply')
-        p1 = p.apply_async(worker1, (100, ))
-        p2 = p.apply_async(worker1, (100, ))
+        r = p.map_async(worker1, [100, 200])
         logging.debug('executed')
-        logging.debug(p1.get())
-        logging.debug(p2.get())
+        logging.debug(r.get())
+
+        # p1 = p.apply_async(worker1, (100, ))
+        # p2 = p.apply_async(worker1, (100, ))
+        # logging.debug('executed')
+        # logging.debug(p1.get())
+        # logging.debug(p2.get())
 
 
