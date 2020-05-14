0b9b45c0f40212808f8061b9dd2540b8a6f29ad4
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-14 19:03:09 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,33 +1,27 @@
 import logging
+import queue
 import threading
 import time
 
 logging.basicConfig(
     level=logging.DEBUG, format='%(threadName)s: %(message)s')
 
-def worker1(semaphore):
-    with semaphore:
-        logging.debug('start')
-        time.sleep(5)
-        logging.debug('end')
+def worker1(queue):
+    logging.debug('start')
+    queue.put(100)
+    time.sleep(5)
+    queue.put(200)
+    logging.debug('end')
 
-def worker2(semaphore):
-    with semaphore:
-        logging.debug('start')
-        time.sleep(5)
-        logging.debug('end')
-
-def worker3(semaphore):
-    with semaphore:
-        logging.debug('start')
-        time.sleep(5)
-        logging.debug('end')
+def worker2(queue):
+    logging.debug('start')
+    logging.debug(queue.get())
+    logging.debug(queue.get())
+    logging.debug('end')
 
 if __name__ == '__main__':
-    semaphore = threading.Semaphore(2)
-    t1 = threading.Thread(target=worker1, args=(semaphore,))
-    t2 = threading.Thread(target=worker2, args=(semaphore,))
-    t3 = threading.Thread(target=worker3, args=(semaphore,))
+    queue = queue.Queue()
+    t1 = threading.Thread(target=worker1, args=(queue,))
+    t2 = threading.Thread(target=worker2, args=(queue,))
     t1.start()
     t2.start()
-    t3.start()
