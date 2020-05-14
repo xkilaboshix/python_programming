6505e9a2fd38a9cbdbf0908ab210d90f6c287aae
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-14 19:38:07 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,36 +1,33 @@
 import logging
-import queue
 import threading
 import time
 
 logging.basicConfig(
     level=logging.DEBUG, format='%(threadName)s: %(message)s')
 
-def worker1(queue):
+def worker1(event):
+    event.wait()
     logging.debug('start')
-    while True:
-        item = queue.get()
-        if item is None:
-            break
-        logging.debug(item)
-        queue.task_done()
-
-    logging.debug('longggggggggggggggggggggggggggggg')
+    time.sleep(3)
     logging.debug('end')
 
-if __name__ == '__main__':
-    queue = queue.Queue()
-    for i in range(100000):
-        queue.put(i)
-    ts = []
-    for _ in range(3):
-        t = threading.Thread(target=worker1, args=(queue,))
-        t.start()
-        ts.append(t)
-    logging.debug('tasks are not done')
-    queue.join()
-    logging.debug('task are done')
-    for _ in range(len(ts)):
-        queue.put(None)
+def worker2(event):
+    event.wait()
+    logging.debug('start')
+    time.sleep(3)
+    logging.debug('end')
 
-    [t.join() for t in ts]
+def worker3(event):
+    logging.debug('start')
+    time.sleep(5)
+    logging.debug('end')
+    event.set()
+
+if __name__ == '__main__':
+    event = threading.Event()
+    t1 = threading.Thread(target=worker1, args=(event,))
+    t2 = threading.Thread(target=worker2, args=(event,))
+    t3 = threading.Thread(target=worker3, args=(event,))
+    t1.start()
+    t2.start()
+    t3.start()
