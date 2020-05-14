748b92a800cdc0207376f08c0f1ce88f2666c72b
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-15 01:49:32 +0900

    up

--- lesson.py
+++ lesson.py
@@ -14,9 +14,9 @@ def worker1(i):
 if __name__ == '__main__':
     # t1 = multiprocessing.Process(target=worker1, args=(i,))
     with multiprocessing.Pool(3) as p:
-        r = p.map_async(worker1, [100, 200])
+        r = p.imap(worker1, [100, 200])
         logging.debug('executed')
-        logging.debug(r.get())
+        logging.debug([i for i in r])
 
         # p1 = p.apply_async(worker1, (100, ))
         # p2 = p.apply_async(worker1, (100, ))
