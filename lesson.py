2331eb36fdeac5099d2e49e14b31d28164da0968
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-15 16:57:52 +0900

    up

--- lesson.py
+++ lesson.py
@@ -5,40 +5,13 @@ import time
 logging.basicConfig(
     level=logging.DEBUG, format='%(processName)s: %(message)s')
 
-def worker1(d, lock):
-    with lock:
-        i = d['x']
-        time.sleep(2)
-        d['x'] = i + 1
-    logging.debug(d)
-
-def worker2(d, lock):
-    with lock:
-        i = d['x']
-        time.sleep(2)
-        d['x'] = i + 1
-    logging.debug(d)
+def f(conn):
+    conn.send(['test'])
+    time.sleep(5)
+    conn.close()
 
 if __name__ == '__main__':
-    d = {'x': 0}
-    lock = multiprocessing.Lock()
-    t1 = multiprocessing.Process(target=worker1, args=(d, lock))
-    t2 = multiprocessing.Process(target=worker1, args=(d, lock))
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-    logging.debug(d)
-    # with multiprocessing.Pool(3) as p:
-    #     r = p.imap(worker1, [100, 200])
-    #     logging.debug('executed')
-    #     for i in r:
-    #         logging.debug(i)
-
-        # p1 = p.apply_async(worker1, (100, ))
-        # p2 = p.apply_async(worker1, (100, ))
-        # logging.debug('executed')
-        # logging.debug(p1.get())
-        # logging.debug(p2.get())
-
-
+    parent_conn, child_conn = multiprocessing.Pipe()
+    p = multiprocessing.Process(target=f, args=(parent_conn, ))
+    p.start()
+    logging.debug(child_conn.recv())
