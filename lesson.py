471cd7aa0919b4d05d0c6736a52da0b01d70b26c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-16 18:07:03 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,36 +1,29 @@
-import logging
+import threading
 import multiprocessing
+
+import concurrent.futures
+import logging
 import time
 
-logging.basicConfig(
-    level=logging.DEBUG, format='%(processName)s: %(message)s')
 
-def worker1(l, d, n):
-    l.reverse()
-    d['x'] += 1
-    n.y += 1
+logging.basicConfig(
+    level=logging.DEBUG, format='%(threadName)s: %(message)s')
+
+def worker(x, y):
+    logging.debug('start')
+    r = x * y
+    logging.debug(r)
+    logging.debug('end')
+    return r
+
+def main():
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        f1 = executor.submit(worker, 2, 5)
+        f2 = executor.submit(worker, 2, 5)
+        logging.debug(f1.result())
+        logging.debug(f2.result())
 
 if __name__ == '__main__':
-    with multiprocessing.Manager() as manager:
-        l = manager.list()
-        d = manager.dict()
-        n = manager.Namespace()
-
-        l.append(1)
-        l.append(2)
-        l.append(3)
-        d['x'] = 0
-        n.y = 0
-
-        p1 = multiprocessing.Process(target=worker1, args=(l, d, n))
-        p2 = multiprocessing.Process(target=worker1, args=(l, d, n))
-        p1.start()
-        p2.start()
-        p1.join()
-        p2.join()
-
-        logging.debug(l)
-        logging.debug(d)
-        logging.debug(n)
+    main()
 
 
