e30798686a7d80f0c1612c26912fb0a6ece35321
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-17 16:06:56 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,3 +1,5 @@
+import base64
+import os
 import hashlib
 
 # print(hashlib.sha256(b'test').hexdigest())
@@ -7,9 +9,12 @@ user_name = 'user1'
 user_pass = 'password'
 db = {}
 
+salt = base64.b64encode(os.urandom(32))
+
 def get_digest(password):
     password = bytes(password, 'utf-8')
-    digest = hashlib.sha256(password).hexdigest()
+    digest = hashlib.sha256(salt + password).hexdigest()
+    print(digest)
     return digest
 
 db[user_name] = get_digest(user_pass)
