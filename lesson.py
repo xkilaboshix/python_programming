805c7dfbdffd1d19d387099c32407936400e0110
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-17 16:16:41 +0900

    up

--- lesson.py
+++ lesson.py
@@ -11,15 +11,23 @@ db = {}
 
 salt = base64.b64encode(os.urandom(32))
 
-def get_digest(password):
-    password = bytes(password, 'utf-8')
-    digest = hashlib.sha256(salt + password).hexdigest()
-    print(digest)
-    return digest
+# def get_digest(password):
+#     password = bytes(password, 'utf-8')
+#     digest = hashlib.sha256(salt + password).hexdigest()
+#     for _ in range(10000):
+#         digest = hashlib.sha256(bytes(digest, 'utf-8')).hexdigest()
+#         print(digest)
+#     print(digest)
+#     return digest
 
-db[user_name] = get_digest(user_pass)
+digest = hashlib.pbkdf2_hmac(
+    'sha256', bytes(user_pass, 'utf-8'), salt, 10000)
+
+db[user_name] = digest
 
 def is_login(user_name, password):
-    return get_digest(password) == db[user_name]
+    digest = hashlib.pbkdf2_hmac(
+        'sha256', bytes(password, 'utf-8'), salt, 10000)
+    return digest == db[user_name]
 
 print(is_login(user_name, user_pass))
