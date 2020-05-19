39abab6854014a9326daa547502daa6acb7fa9aa
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-20 00:23:39 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,37 +1,9 @@
 import collections
 
-d = {}
-l = ['a', 'a', 'a', 'b', 'b', 'c']
-for word in l:
-    if word not in d:
-        d[word] = 0
-    d[word] += 1
-print(d)
 
+d = {'apple': 4, 'banana': 3, 'pear': 1, 'orange': 2}
 
-d = {}
-l = ['a', 'a', 'a', 'b', 'b', 'c']
-for word in l:
-    d.setdefault(word, 0)
-    d[word] += 1
-print(d)
+od = collections.OrderedDict(
+    sorted(d.items(), key=lambda t: t[0]))
+print(od)
 
-
-d = collections.defaultdict(int)
-l = ['a', 'a', 'a', 'b', 'b', 'c']
-for word in l:
-   d[word] += 1
-print(d)
-
-l = ['a', 'a', 'a', 'b', 'b', 'c']
-c = collections.Counter()
-for word in l:
-    c[word] += 1
-print(c)
-print(c.most_common(2))
-print(sum(c.values()))
-
-import re
-with open('lesson.py', 'r') as f:
-    words = re.findall(r'\w+', f.read().lower())
-    print(collections.Counter(words).most_common(20))
