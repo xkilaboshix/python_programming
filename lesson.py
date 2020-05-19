fdb427d412a508fecb51932942181bbd6e76bfea
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-19 18:08:17 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,27 +1,31 @@
-import io
-import requests
-import zipfile
+import collections
 
 
-# with open('/tmp/a.txt', 'w') as f:
-#     f.write('test test')
-#
-# with open('/tmp/a.txt', 'r') as f:
-#     print(f.read())
-#
-# f = io.BytesIO()
-# f.write(b'string io test')
-# f.seek(0)
-# print(f.read())
+a = {'a': 'a', 'c': 'c', 'num': 0}
+b = {'b': 'b', 'c': 'cc'}
+c = {'b': 'bbb', 'c': 'ccc'}
 
-url = ('https://files.pythonhosted.org/packages/ac/d6/0f6c0d9d0b07bbb2085e94a71aded1e1'
-       '37c7c9002ac54924bc1c0adf748a/setuptools-46.4.0.zip')
+class DeepChainMap(collections.ChainMap):
+    def __setitem__(self, key, value):
+        for mapping in self.maps:
+            if type(mapping[key]) is int and mapping[key] < value:
+                mapping[key] = value
+            return
+        self.maps[0][key] = value
 
-f = io.BytesIO()
+m = DeepChainMap(a, b, c)
+m['num'] = -1
+print(m['num'])
 
-r = requests.get(url)
-f.write(r.content)
+# print(a)
+# a.update(b)
+# print(a)
+# a.update(c)
+# print(a)
 
-with zipfile.ZipFile(f) as z:
-       with z.open('setuptools-46.4.0/README.rst') as r:
-              print(r.read().decode())
+# m = collections.ChainMap(a, b, c)
+# print(m)
+# print(m.maps)
+# m.maps.reverse()
+# print(m.maps)
+# print(m['c'])
