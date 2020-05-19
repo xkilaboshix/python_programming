fca26087f77f973f76dd64b786286859ae329a09
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-19 20:18:19 +0900

    up

--- lesson.py
+++ lesson.py
@@ -22,15 +22,16 @@ l = ['a', 'a', 'a', 'b', 'b', 'c']
 for word in l:
    d[word] += 1
 print(d)
-d['d'] = 6
-print(d['b'])
-print(d)
 
+l = ['a', 'a', 'a', 'b', 'b', 'c']
+c = collections.Counter()
+for word in l:
+    c[word] += 1
+print(c)
+print(c.most_common(2))
+print(sum(c.values()))
 
-d = collections.defaultdict(set)
-s = [('red', 1), ('blue', 2), ('red', 3), ('blue', 4),
-     ('red', 1), ('blue', 4)]
-for k, v in s:
-    d[k].add(v)
-print(d)
-print(d['red'])
+import re
+with open('lesson.py', 'r') as f:
+    words = re.findall(r'\w+', f.read().lower())
+    print(collections.Counter(words).most_common(20))
