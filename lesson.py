a2355bfccbdbcf428ebdaf80bda53ecf5365eb42
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-19 17:06:56 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,15 +1,27 @@
-import contextlib
+import io
+import requests
+import zipfile
 
-def tag(f):
-    def _wrapper():
-        print('<h2>')
-        r = f()
-        print('<h2>')
-        return r
-    return _wrapper
 
-@tag
-def f():
-    print('test')
+# with open('/tmp/a.txt', 'w') as f:
+#     f.write('test test')
+#
+# with open('/tmp/a.txt', 'r') as f:
+#     print(f.read())
+#
+# f = io.BytesIO()
+# f.write(b'string io test')
+# f.seek(0)
+# print(f.read())
 
-f()
+url = ('https://files.pythonhosted.org/packages/ac/d6/0f6c0d9d0b07bbb2085e94a71aded1e1'
+       '37c7c9002ac54924bc1c0adf748a/setuptools-46.4.0.zip')
+
+f = io.BytesIO()
+
+r = requests.get(url)
+f.write(r.content)
+
+with zipfile.ZipFile(f) as z:
+       with z.open('setuptools-46.4.0/README.rst') as r:
+              print(r.read().decode())
