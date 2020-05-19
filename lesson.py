0ba359ef9ad3556133cb2b8ae8320dd34250029e
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-19 15:01:38 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,33 +1,15 @@
-import base64
-import os
-import hashlib
+import contextlib
 
-# print(hashlib.sha256(b'test').hexdigest())
-# print(hashlib.sha256(b'test').hexdigest())
+def tag(f):
+    def _wrapper():
+        print('<h2>')
+        r = f()
+        print('<h2>')
+        return r
+    return _wrapper
 
-user_name = 'user1'
-user_pass = 'password'
-db = {}
+@tag
+def f():
+    print('test')
 
-salt = base64.b64encode(os.urandom(32))
-
-# def get_digest(password):
-#     password = bytes(password, 'utf-8')
-#     digest = hashlib.sha256(salt + password).hexdigest()
-#     for _ in range(10000):
-#         digest = hashlib.sha256(bytes(digest, 'utf-8')).hexdigest()
-#         print(digest)
-#     print(digest)
-#     return digest
-
-digest = hashlib.pbkdf2_hmac(
-    'sha256', bytes(user_pass, 'utf-8'), salt, 10000)
-
-db[user_name] = digest
-
-def is_login(user_name, password):
-    digest = hashlib.pbkdf2_hmac(
-        'sha256', bytes(password, 'utf-8'), salt, 10000)
-    return digest == db[user_name]
-
-print(is_login(user_name, user_pass))
+f()
