f5e0bc5abd5a67614a674629f71dc745a0335629
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-21 02:03:56 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,25 +1,15 @@
-import datetime
+import json
+import pprint
 
-
-# d = datetime.datetime.now()
-# print(d)
-# print(str(d))
-# print(repr(d))
-
-# print('{!r} {} {!s} '.format('test', 'test1', 'test2'))
-
-class Point(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __repr__(self):
-        return 'Point<object>'
-
-    def __str__(self):
-        return 'Point ({}, {})'.format(self.x, self.y)
-
-p = Point(100, 200)
-print('{0!r}'.format(p))
-print('{0}'.format(p))
-print('{0!s}'.format(p))
+l = ['apple', 'orange', 'banana', 'peach', 'mango']
+l.insert(0, l[:])
+# l.insert(0, l[:])
+# l.insert(0, l[:])
+# l.insert(0, l[:])
+#
+# print(l)
+#
+# pp = pprint.PrettyPrinter(
+#     indent=4, width=40, compact=True
+# )
+# pp.pprint(l)
