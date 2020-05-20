e688cb92e5615141e671832f3576d0aacbbeef7c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-20 20:15:32 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,23 +1,8 @@
-import re
-"""
-match()    文字列の先頭で正規表現とマッチするか判定
-search()   文字列を操作して、正規表現がどこにマッチするか調べる
-findall()  正規表現にマッチする部分文字列を全て探し出しリストとして返す
-finditer() 重複しないマッチングオブジェクトのイテレータを返す
-"""
-# m = re.match('a.c', 'abc')
-# print(m.group())
+print(int(100), hex(100), oct(100), bin(100))
+print('{0:d} {0:#x} {0:#o} {0:#b}'.format(100))
 
-# m = re.search('a.c', 'test abc test')
-# print(m)
-# print(m.span())
-# print(m.group())
 
-# m = re.findall('a.c', 'test abc test abc')
-# print(m)
-
-# m = re.finditer('a.c', 'test abc test abc')
-# print([w.group() for w in m])
-
-m = re.match('ab?', 'abb')
-print(m)
+for i in range(20):
+    for base in 'bdX':
+        print('{:5{base}}'.format(i, base=base), end=' ')
+    print()
