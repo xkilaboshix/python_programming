e19722be12ad531d2fb3c241318cb64082e6b7c7
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-21 02:15:02 +0900

    up

--- lesson.py
+++ lesson.py
@@ -3,13 +3,13 @@ import pprint
 
 l = ['apple', 'orange', 'banana', 'peach', 'mango']
 l.insert(0, l[:])
-# l.insert(0, l[:])
-# l.insert(0, l[:])
-# l.insert(0, l[:])
-#
-# print(l)
-#
-# pp = pprint.PrettyPrinter(
-#     indent=4, width=40, compact=True
-# )
-# pp.pprint(l)
+l.insert(0, l[:])
+l.insert(0, l[:])
+l.insert(0, l[:])
+
+print(l)
+
+pp = pprint.PrettyPrinter(
+    indent=4, width=40, compact=True, depth=3
+)
+pp.pprint(l)
