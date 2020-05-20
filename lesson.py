67dea274ea370edc67fe49313e757bfbf0ae0a6b
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-21 01:36:42 +0900

    repr, str

--- lesson.py
+++ lesson.py
@@ -1,8 +1,25 @@
-print(int(100), hex(100), oct(100), bin(100))
-print('{0:d} {0:#x} {0:#o} {0:#b}'.format(100))
+import datetime
 
 
-for i in range(20):
-    for base in 'bdX':
-        print('{:5{base}}'.format(i, base=base), end=' ')
-    print()
+# d = datetime.datetime.now()
+# print(d)
+# print(str(d))
+# print(repr(d))
+
+# print('{!r} {} {!s} '.format('test', 'test1', 'test2'))
+
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Point<object>'
+
+    def __str__(self):
+        return 'Point ({}, {})'.format(self.x, self.y)
+
+p = Point(100, 200)
+print('{0!r}'.format(p))
+print('{0}'.format(p))
+print('{0!s}'.format(p))
