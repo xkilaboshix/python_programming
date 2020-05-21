c09209dff498f37e4e6dd9a3f967dc3de2bdcb92
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-21 16:53:04 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,10 +1,20 @@
 import enum
 
 
+db = {
+    'stack1': 1,
+    'stack2': 2,
+}
+
 class Status(enum.Enum):
     ACTIVE = 1
     INACTIVE = 2
     RUNNING = 3
+
+if Status(db['stack1']) == Status.ACTIVE:
+    print('shutdown')
+elif Status(db['stack1']) == Status.INACTIVE:
+    print('terminate')
 
 # print(Status.ACTIVE)
 # print(Status(2))
