ffe8303fe5443925b9d6fa08a33650977dfc8fe7
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-21 18:03:53 +0900

    functools.wraps

--- lesson.py
+++ lesson.py
@@ -1,27 +1,19 @@
-# def memoize(f):
-#     memo = {}
-#     def _wrapper(n):
-#         if n not in memo:
-#             memo[n] = f(n)
-#             print('hit')
-#             print(memo)
-#         return memo[n]
-#     return _wrapper
-
 import functools
 
-@functools.lru_cache(maxsize=5)
-def long_func(n):
-    r = 0
-    for i in range(10000000):
-        r += n * i
-    return r
 
-for i in range(10):
-    print(long_func(i))
+def d(f):
+    @functools.wraps(f)
+    def wrapper():
+        """Wrapper docstring"""
+        print('くぁwせdrftgyふじこlp')
+        return f()
+    return wrapper
 
-print(long_func.cache_info())
+@d
+def shimarin():
+    """shimarin docstring"""
+    print('死んじまったじゃねえか')
 
-print('next run')
-for i in reversed(range(10)):
-    print(long_func(i))
+# shimarin()
+print(shimarin.__doc__)
+# help(shimarin)
