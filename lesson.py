8b4f6f2f97f7471663a2360aabf3c2b7fd44531a
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-22 01:54:52 +0900

    tkinter

--- lesson.py
+++ lesson.py
@@ -1,38 +1,25 @@
-import turtle
+import tkinter as tk
 
-turtle.pencolor('orange')
-for i in range(200):
-    turtle.fd(i)
-    turtle.left(360/4 + 20)
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
-# turtle.pencolor('green')
-#
-# for i in range(60):
-#     turtle.fd(50)
-#     turtle.left(360/3 + 10)
-#
-# turtle.pencolor('red')
-#
-# for i in range(60):
-#     turtle.fd(200)
-#     turtle.left(360/3 + 10)
-#
-# turtle.color('red', 'yellow')
-#
-# turtle.begin_fill()
-# for i in range(5 * 3):
-#     turtle.forward(100 + i * 10)
-#     turtle.right(360 / 5 * 2)
-# turtle.end_fill()
-#
-# turtle.done()
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
 
-# turtle.color('red', 'yellow')
-#
-# turtle.begin_fill()
-# for _ in range(4):
-#     turtle.forward(100)
-#     turtle.right(90)
-# turtle.end_fill()
-#
-turtle.done()
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
