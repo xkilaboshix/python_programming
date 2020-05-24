826738eb5dcffde5a3deea325364403073941703
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-25 01:08:52 +0900

    ZeroMQのPUSHとPULL

--- client2.py
+++ client2.py
@@ -1,14 +1,10 @@
-from multiprocessing.managers import BaseManager
+import zmq
 
 
-class QueueManager(BaseManager):
-    pass
+context = zmq.Context()
+sock = context.socket(zmq.PULL)
+sock.connect("tcp://127.0.0.1:5690")
 
-QueueManager.register('get_queue')
-
-manager = QueueManager(
-    address=('127.0.0.1', 50000),
-    authkey=b'abracadabra')
-manager.connect()
-queue = manager.get_queue()
-print(queue.get())
+while True:
+    message = sock.recv()
+    print("Received: {}".format(message.decode()))
