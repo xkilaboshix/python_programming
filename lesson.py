122a4c5ecf5fa76fb7a91e2882b540ba8445e943
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-28 14:33:25 +0900

    up

--- lesson.py
+++ lesson.py
@@ -1,7 +1,15 @@
-def g_hello():
-    yield 'hello 1'
-    yield 'hello 2'
-    yield 'hello 3'
+# def g_hello():
+#     yield 'hello 1'
+#     yield 'hello 2'
+#     yield 'hello 3'
+#
+# for g in g_hello():
+#     print(g)
 
-for g in g_hello():
-    print(g)
+def g_hello():
+    r = yield 'hello'
+    yield r
+
+g = g_hello()
+print(next(g))
+print(next(g))
