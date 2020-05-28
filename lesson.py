8829460378a41f83374b7636883cdbcedc16c18c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-28 17:32:02 +0900

    asyncio.Condition

--- lesson.py
+++ lesson.py
@@ -4,26 +4,30 @@ import asyncio
 loop = asyncio.get_event_loop()
 
 
-async def worker1(event):
-    print('worker1 start')
-    await event.wait()
-    print('worker1 got event')
-    await asyncio.sleep(3)
-    print('worker1 end')
+async def worker1(condition):
+    async with condition:
+        await condition.wait()
+        print('worker1 start')
+        print('worker1 got condition')
+        await asyncio.sleep(3)
+        print('worker1 end')
 
-async def worker2(event):
-    print('worker2 start')
-    await event.wait()
-    print('worker2 got event')
-    await asyncio.sleep(3)
-    print('worker2 end')
+async def worker2(condition):
+    async with condition:
+        await condition.wait()
+        print('worker2 start')
+        print('worker2 got condition')
+        await asyncio.sleep(3)
+        print('worker2 end')
 
-async def worker3(event):
-    print('worker3 start')
-    await asyncio.sleep(3)
-    print('worker3 end')
-    event.set()
+async def worker3(condition):
+    await asyncio.sleep(1)
+    async with condition:
+        print('worker3 start')
+        await asyncio.sleep(3)
+        print('worker3 end')
+        condition.notify_all()
 
-event = asyncio.Event()
-loop.run_until_complete(asyncio.wait([worker1(event), worker2(event), worker3(event)]))
+condition = asyncio.Condition()
+loop.run_until_complete(asyncio.wait([worker1(condition), worker2(condition), worker3(condition)]))
 loop.close()
