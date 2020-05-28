54d05608864596de0925f5e84dd022c0e100a90f
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-29 01:01:34 +0900

    asyncio.Queue

--- lesson.py
+++ lesson.py
@@ -4,30 +4,18 @@ import asyncio
 loop = asyncio.get_event_loop()
 
 
-async def worker1(condition):
-    async with condition:
-        await condition.wait()
-        print('worker1 start')
-        print('worker1 got condition')
-        await asyncio.sleep(3)
-        print('worker1 end')
+async def worker1(queue):
+    print('worker1 start')
+    await queue.put(100)
+    print('worker1 end')
 
-async def worker2(condition):
-    async with condition:
-        await condition.wait()
-        print('worker2 start')
-        print('worker2 got condition')
-        await asyncio.sleep(3)
-        print('worker2 end')
+async def worker2(queue):
+    print('worker2 start')
+    x = await queue.get()
+    print(x)
+    print('worker2 end')
 
-async def worker3(condition):
-    await asyncio.sleep(1)
-    async with condition:
-        print('worker3 start')
-        await asyncio.sleep(3)
-        print('worker3 end')
-        condition.notify_all()
 
-condition = asyncio.Condition()
-loop.run_until_complete(asyncio.wait([worker1(condition), worker2(condition), worker3(condition)]))
+queue = asyncio.Queue()
+loop.run_until_complete(asyncio.wait([worker1(queue), worker2(queue)]))
 loop.close()
