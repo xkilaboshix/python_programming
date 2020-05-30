e39ae05e6e23328f1948f6953389b5c12f93f9bc
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-05-31 01:14:13 +0900

    asyncio.Queue

--- lesson.py
+++ lesson.py
@@ -4,18 +4,13 @@ import asyncio
 loop = asyncio.get_event_loop()
 
 
-async def worker1(queue):
-    print('worker1 start')
-    await queue.put(100)
-    print('worker1 end')
-
-async def worker2(queue):
-    print('worker2 start')
-    x = await queue.get()
-    print(x)
-    print('worker2 end')
+async def f(future):
+    await asyncio.sleep(1)
+    future.set_result('Future is done!')
 
 
-queue = asyncio.Queue()
-loop.run_until_complete(asyncio.wait([worker1(queue), worker2(queue)]))
+future = asyncio.Future()
+asyncio.ensure_future(f(future))
+loop.run_until_complete(future)
+print(future.result())
 loop.close()
